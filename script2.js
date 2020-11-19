d88a9931139f18d721f3e49666f5f2819ccc7377
thedukeofcool <maybinullo@icloud.com>
2020-11-19 06:35:38 -0500

    Added Database

--- script2.js
+++ script2.js
@@ -1,10 +1,30 @@
 let url = location.href;
 let bookName = url.substr(url.lastIndexOf("?name=")+6);
-bookName = bookName.replaceAll('%20', ' '); 
+bookName = bookName.replaceAll('%20', ' ');
 bookName = bookName.replaceAll('%22', '\"');
 bookName = bookName.replaceAll('%27', "\'")
 document.getElementById("search").value = bookName;
 
+var database = ["Dog Man: Unleased", "Dog Man: A Tale of Two Kittens", "Dog Man: Lord Of the Fleas", "Dog Man: Brawl of the Wild", "Dog man: For Whom the Ball Rolls", "Dog Man: Fetch-22", "Dog Man: Grime and Punishment"];
+
+function addText(text, link)
+{
+    var r2 = document.createElement("A");
+    var r = document.createElement("P");
+    r.textContent = text;
+    r2.href = link;
+    r2.appendChild(r);
+    document.getElementById("results").appendChild(r2);
+}
+
+if (bookName == "Dog Man")
+{
+    for (let i = 0; i < 6; i++)
+    {
+        addText(database[i], "https://google.com");
+    }
+}
+
 function findBook() {
-  window.location.href = "search.html?name=" + document.getElementById("search").value
+    window.location.href = "search.html?name=" + document.getElementById("search").value
 }
